f4dd5d96b0d875a4c2e13be7747150824105e0b6
Emily Lemmon <eclaire.lemmon@gmail.com>
2017-09-23 11:01:58 -0500

    Add some files to the initial setup

---
+++ js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+//I think we can use jQuery as a CDN instead of installing it
+
+// Need to collect info from the sign up process from the HTML page
+// Store in FB
+
+// When a farmer clicks the button, (**get all data from optional fields and)
+// Save details to DB including timestamp and
+// Send email to sosatn@endhunger.org

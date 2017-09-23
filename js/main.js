0c961104913cc020a7aeb5d42a2bbea73303d145
Emily Lemmon <eclaire.lemmon@gmail.com>
2017-09-23 12:59:36 -0500

    add function for input capture and FB ajax call to POST

--- js/main.js
+++ js/main.js
@@ -2,19 +2,68 @@
 
 // URL for FB for farmer profile posting: https://gleantn-1794b.firebaseio.com/USER/Farmer
 
-
-
-let $uName = $('#nameInput');
-let $uAddress = $('#addressInput');
-let $uPhone = $('#phoneInput');
-
-//I think we can use jQuery as a CDN instead of installing it
+let $ = require('jquery');
+let FBurl = 'https://gleantn-1794b.firebaseio.com/USER/Farmer';
+let firebase = $require('FBconfig.js');
 
 
 
+$("#register-btn").click( () => {
+	let $uName = $('#name');
+	let $uAddress = $('#streetname');
+	let $uPhone = $('#phone');
+	let $uCity = $('#city');
+	let $uState = $('#state');
+	let $uZip = $('#zip');
+	addFarmerProfile();
+});
 // Need to collect info from the sign up process from the HTML page
 // Store in FB
 
 // When a farmer clicks the button, (**get all data from optional fields and)
 // Save details to DB including timestamp and
 // Send email to sosatn@endhunger.org
+
+
+//on click of "register" button, capture what's in fields and store as object, send to FB, then send to next page
+
+
+addFarmerProfile = (farmerObj) => { 
+	let currentUser = firebase.auth().currentUser.uid;
+	return new Promise ( (resolve, reject) => {
+			console.log("current User", currentUser);
+			farmerObj.uid = currentUser;
+			// farmerObj.name = $uName;
+			// farmerObj.street = $uAddress;
+			// farmerObj.city = $uCity;
+			// farmerObj.state = $uState;
+			// farmerObj.zip = $uZip;
+			// farmerObj.phone = $uPhone;
+			// farmerObj.email = $uEmail;
+			$.ajax({
+				url: `${FBurl}.json`,
+				type: "POST",
+				data: JSON.stringify(farmerObj),
+				dataType: 'json'
+			}).done( (data) => {
+				resolve(data);
+			}).fail ( (error) => {
+				console.log("Error");
+			});
+
+		});
+
+};
+
+let tempObj = {
+	name = 'Emily'
+	street = '123 Sesame',
+	city = 'Woodstock',
+	state = 'Georgia',
+	zip = 23456,
+	phone = '555-666-7777',
+	email = 'em@lem.com'
+}
+
+
+addFarmerProfile(tempObj);
